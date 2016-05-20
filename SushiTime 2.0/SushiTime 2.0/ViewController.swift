64b86d2c3ebe4de9c87b9bbb4052e96b44b2f9a2
rsgc-blackwell-s <student@MacBook-Pro443-Blackwell-2.local>
2016-05-20 10:21:43 -0400

    Adding IBOutlets/IBActions, begin to call API

--- SushiTime 2.0/SushiTime 2.0/ViewController.swift
+++ SushiTime 2.0/SushiTime 2.0/ViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityTempLabel: UILabel!
+    
+    @IBAction func getDataButton(sender: AnyObject) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9aa3efef6de3b5bd8331807ab02d36cf")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getWeatherData(urlString: String){
+        let url = NSURL(string: urlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) in
+            
+        }
+    }
 
+    func setLabels(weatherData: NSData){
+        
+    }
 }
 
