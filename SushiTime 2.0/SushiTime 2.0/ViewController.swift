56d7ed5b06877e9c97b0b55f97ffda6d13d9ac2f
rsgc-blackwell-s <student@MacBook-Pro443-Blackwell-2.local>
2016-05-22 17:40:23 -0400

    Finished what I could, stuck on a syntax error...
    Probably an issue with Xcode 2.0 conversion; sources I used Xcode 1.1.

--- SushiTime 2.0/SushiTime 2.0/ViewController.swift
+++ SushiTime 2.0/SushiTime 2.0/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var cityNameTextField: UITextField!
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var cityTempLabel: UILabel!
     
     @IBAction func getDataButton(sender: AnyObject) {
+        getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=\(cityNameTextField.text)")
     }
 
     override func viewDidLoad() {
@@ -28,16 +29,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func getWeatherData(urlString: String){
+    func getWeatherData(urlString: String) {
         let url = NSURL(string: urlString)
         
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) in
-            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.setLabels(data!)
+            })
         }
+        task.resume()
     }
 
     func setLabels(weatherData: NSData){
+        var jsonError: NSError?
         
+        let json = NSJSONSerialization.JSONObjectWithData(weatherData, options: [], error: &jsonError) as NSDictionary
+    
+        if let name = json["name"] as? String {
+            cityNameLabel.text = name
+        }
+        
+        if let main = json["main"] as NSDictionary {
+            if let temp = main["temp"] as? Double {
+                //add 273.15
+                cityTempLabel.text = String(format: "%.1f", temp)
+                
+            }
+        }
     }
 }
+
 
