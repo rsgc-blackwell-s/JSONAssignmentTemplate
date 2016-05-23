70cacb8c4b5d0ba3d25336486e330440241a4397
rsgc-blackwell-s <student@MacBook-Pro443-Blackwell-2.local>
2016-05-23 16:46:47 -0400

    Made some comments...
    Also: Still unable to fix my bug :(

--- SushiTime 2.0/SushiTime 2.0/ViewController.swift
+++ SushiTime 2.0/SushiTime 2.0/ViewController.swift
@@ -10,18 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //outlets
     @IBOutlet weak var cityNameTextField: UITextField!
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var cityTempLabel: UILabel!
     
+    //if button pressed: get localized weather (based on user input)
     @IBAction func getDataButton(sender: AnyObject) {
         getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=\(cityNameTextField.text)")
     }
 
+    //call for API data
     override func viewDidLoad() {
         super.viewDidLoad()
         getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9aa3efef6de3b5bd8331807ab02d36cf")
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,28 +31,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //get weather data in String
     func getWeatherData(urlString: String) {
-        let url = NSURL(string: urlString)
+        let url = NSURL(string: urlString) //creating URL object
         
+        //http request
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) in
             dispatch_async(dispatch_get_main_queue(), {
                 self.setLabels(data!)
             })
         }
-        task.resume()
+        
+        task.resume() //debug - start task from its suspension state
     }
 
+    //setting labels
     func setLabels(weatherData: NSData){
         var jsonError: NSError?
         
+        //should return as structure (to be parsed as NSDictionary...)
         let json = NSJSONSerialization.JSONObjectWithData(weatherData, options: [], error: &jsonError) as NSDictionary
-    
-        if let name = json["name"] as? String {
+        
+        //CITY
+        //if we get a value, the name perameter will be set
+        if let name = json["name"] as? String { //parsing (extraction city name)
             cityNameLabel.text = name
         }
         
+        //TEMP
+        //
         if let main = json["main"] as NSDictionary {
-            if let temp = main["temp"] as? Double {
+            if let temp = main["temp"] as? Double { //parsing (extracting temp)
                 //add 273.15
                 cityTempLabel.text = String(format: "%.1f", temp)
                 
